subsys0_3 - fixed forehead detection with derived formula

EE_team/multipurpose_cam_main/subsys0_3.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import time
+import numpy as np
 
 thermal_device_id = 2
 # IMG_BUFFER_DISCARD = 1
@@ -62,11 +63,6 @@ def run(displaymode,verbose,run_openpose,write_images,openpose_device_id):
             time_imcap_st_fi = time.time()
 
             if run_openpose:
-                # images_discarded_from_buffer = 0
-
-                # time_comp_th_be=time.time()
-                # compute_openpose(opWrapper,datum_thermal,thermalImageToProcess)
-                # time_comp_th_fi=time.time()
 
                 time_comp_st_be=time.time()
                 datum.cvInputData = imageToProcess
@@ -74,19 +70,46 @@ def run(displaymode,verbose,run_openpose,write_images,openpose_device_id):
                 time_comp_st_fi=time.time()
 
                 if write_images:
-                    print()
-                    #cv2.imwrite(image_path+"op.png",datum_stereo.cvOutputData)
-                    # cv2.imwrite(image_path+"th.png",datum_thermal.cvOutputData)
+                    cv2.imwrite(image_path+"op.png",datum.cvOutputData)
+
+                # if people detected, calculate their eyes/nose. use this to calculate forehead
+                if datum.poseKeypoints is not None:
+                    person_num = 0
+                    foreheads = [0] * len(datum.poseKeypoints)
+
+                    for person in datum.poseKeypoints:
+                        # according to default body_25 model, keypoint0 is nose, keypoint15/16 are eyes
+                        # keypoint is made up of (xcoord,ycoord,confidence). we ignore confidence
+                        nose = person[0][0:-1]
+                        key_eye_R = person[15][0:-1]
+                        key_eye_L = person[16][0:-1]
+
+                        if verbose:
+                            print("person "+ str(person_num) +"'s nose coords:" + str(nose))
+                            print("person "+ str(person_num) +"'s eyeL coords:" + str(key_eye_L))
+                            print("person "+ str(person_num) +"'s eyeR coords:" + str(key_eye_R))
+
+                        #math is included in report
+                        eyes_midpoint = np.array([key_eye_R[0]+(key_eye_L[0]-key_eye_R[0])/2,
+                                key_eye_L[1]+(key_eye_R[1]-key_eye_L[1])/2])
+                        
+                        eyes_midpoint_to_nose_scalefactor = 1.6
+                        forehead = eyes_midpoint-eyes_midpoint_to_nose_scalefactor*(nose-eyes_midpoint)
+
+                        foreheads[person_num] = forehead
+
+                        person_num = person_num + 1
+
+                    print("foreheads:",foreheads)
 
             if verbose:
                 print()
-                #print("Body keypoints: \n" + str(datum_stereo.poseKeypoints))
-                # print("Thermal keypoints: \n" + str(datum_thermal.poseKeypoints))
+                print("Body keypoints: \n" + str(datum.poseKeypoints))
 
             # Display Image
             if displaymode: 
                     # the following comments assume subject is about 0.5 m from camera
-                x_offset = 300 #the following values were experimentally obtained
+                x_offset = 260 #the following values were experimentally obtained
                 y_offset = 125 #seems to work on left camera with respective values:
                 x_scale = 5.65    #280,160,6,4
                 y_scale = 4.5
@@ -100,8 +123,17 @@ def run(displaymode,verbose,run_openpose,write_images,openpose_device_id):
                         print("thermal img shape",thermalImageToProcess.shape)
 
                     img_combined[y_offset:y_offset+thermalImageToProcess.shape[0],x_offset:x_offset+thermalImageToProcess.shape[1]] = thermalImageToProcess
+                    
+                    # add forehead marker (if applicable)
+                    if run_openpose:
+                        forehead_marker_color = (0,0,0)
+
+                        for person_forehead in foreheads:
+                            cv2.drawMarker(img_combined,(int(person_forehead[0]),int(person_forehead[1])), forehead_marker_color,
+                             markerType=cv2.MARKER_DIAMOND, markerSize=10, thickness=10, line_type=cv2.LINE_AA)
                     cv2.imshow("output display",img_combined)
-                    time_show_fi = time.time() 
+                    time_show_fi = time.time()
+
                     if verbose:
                         delay_imcap_th = time_imcap_th_fi-time_imcap_th_be
                         delay_imcap_st = time_imcap_st_fi-time_imcap_st_be
@@ -147,6 +179,9 @@ def run(displaymode,verbose,run_openpose,write_images,openpose_device_id):
                         print("\t\tTHERMAL DELAY:",delay_imcap_th)
                         print("\t\tSTEREO DELAY:",delay_imcap_st)
                         print("\t\tTOTAL DELAY:",delay_imcap_th+delay_show+delay_imcap_st)
+
+                if write_images:
+                    cv2.imwrite(image_path+"th+op.png",img_combined)
 
                 key = cv2.waitKey(1)
 